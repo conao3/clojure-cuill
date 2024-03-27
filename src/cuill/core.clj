795512aa541227172967f520a966db7fedcdb7a2
Naoya Yamashita <conao3@gmail.com>
2024-03-27 23:36:04 +0900

    add setCorsorPosition sample

--- src/cuill/core.clj
+++ src/cuill/core.clj
@@ -2,8 +2,8 @@
   (:import [com.googlecode.lanterna.terminal Terminal DefaultTerminalFactory]))
 
 (defn -main [& args]
-  (println "Hello, World!")
-  (let* [default-terminal-factory (DefaultTerminalFactory.)
+  ;; (println "Hello, World!")
+  (let* [default-terminal-factory (new DefaultTerminalFactory)
          terminal (.createTerminal default-terminal-factory)]
 
     (do
@@ -12,8 +12,16 @@
      (.putCharacter terminal \l)
      (.putCharacter terminal \l)
      (.putCharacter terminal \o)
-     (.putCharacter terminal \!)
+     (.putCharacter terminal \newline)
      (.flush terminal))
+
+    (Thread/sleep 2000)
+
+    (let [start-position (.getCursorPosition terminal)]
+      (.setCursorPosition terminal (-> start-position
+                                       (.withRelativeColumn 3)
+                                       (.withRelativeRow 2)))
+      (.flush terminal))
 
     (.readInput terminal)
     (.close terminal)))
