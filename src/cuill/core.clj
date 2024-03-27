6fb0116a087dcf3d156186c959c7fbea558ced35
Naoya Yamashita <conao3@gmail.com>
2024-03-27 23:12:25 +0900

    frist lanterna

--- src/cuill/core.clj
+++ src/cuill/core.clj
@@ -1,4 +1,19 @@
-(ns cuill.core)
+(ns cuill.core
+  (:import [com.googlecode.lanterna.terminal Terminal DefaultTerminalFactory]))
 
 (defn -main [& args]
-  (println "Hello, World!"))
+  (println "Hello, World!")
+  (let* [default-terminal-factory (DefaultTerminalFactory.)
+         terminal (.createTerminal default-terminal-factory)]
+
+    (do
+     (.putCharacter terminal \H)
+     (.putCharacter terminal \e)
+     (.putCharacter terminal \l)
+     (.putCharacter terminal \l)
+     (.putCharacter terminal \o)
+     (.putCharacter terminal \!)
+     (.flush terminal))
+
+    (.readInput terminal)
+    (.close terminal)))
